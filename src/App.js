65ed8751d23da809569f427b5799e422a808ee90
andrasat <andra.satria1@gmail.com>
2017-06-18 23:51:42 +0700

    working on web

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
+
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      evKey: 0
+    };
+  }
+
+  _handleSelect(evKey) {
+    this.setState({evKey});
+  };
+
   render() {
+    const { evKey } = this.state
     return (
       <div className="App">
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Carolus Summarecon</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav bsStyle="tabs" activeKey={evKey} onSelect={(e) => this._handleSelect(e)} pullRight={true}>
+            <NavItem eventKey="1">Profile</NavItem>
+            <NavItem eventKey="2">Visi & Misi</NavItem>
+            <NavItem eventKey="3">Kontak</NavItem>
+            <NavItem eventKey="4">Guest Book</NavItem>
+          </Nav>
+        </Navbar>
         <h1>Hello World</h1>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+const store = createStore(reducers);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'));
 registerServiceWorker();
