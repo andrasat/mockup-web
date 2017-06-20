6ab53939fa3e98c3582e6a481b692f6955bffd23
andrasat <andra.satria1@gmail.com>
2017-06-20 16:18:35 +0700

    working on landing page

--- src/App.js
+++ src/App.js
@@ -5,10 +5,36 @@ import './App.css';
 
 
 
-class App extends React.Component {
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      height: 0,
+      width: 0
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this._resizeWindow);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this._resizeWindow);
+  }
+
+  _resizeWindow = () => {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+  }
+
+  _isActive = () => {
+
+  }
 
   render() {
-    // const { } = this.state
+    const { width, height } = this.state;
     const settings = {
       dots: true,
       infinite: true,
@@ -17,7 +43,7 @@ class App extends React.Component {
       speed: 850,
       slidesToShow: 1,
       slidesToScroll: 1
-    }
+    };
     return (
       <Router>
         <div className="App">
@@ -25,19 +51,34 @@ class App extends React.Component {
             <div className="columns is-mobile">
               <div className="column is-6">
                 <Link className="title is-1 carolus" to="/">St. Carolus</Link>
-                <p id="mini-text">Summarecon Serpong</p>
+                <p className="mini-text">Summarecon Serpong</p>
               </div>
             </div>
           </div>
           <nav className="nav hospital">
             <div className="nav-left">
-              <p className="nav-item"><Link className="left-items" to="/profile">Profile</Link></p>
-              <p className="nav-item"><Link className="left-items" to="/visi&misi">Visi & Misi</Link></p>
-              <p className="nav-item"><Link className="left-items" to="/kontak">Kontak</Link></p>
+            <p className="nav-item is-hidden-mobile"><Link className="left-items" to="/news">News</Link></p>
+            <p className="nav-item is-hidden-mobile"><Link className="left-items" to="/profile">Profile</Link></p>
+            <p className="nav-item is-hidden-mobile"><Link className="left-items" to="/visi&misi">Visi & Misi</Link></p>
+            <p className="nav-item is-hidden-mobile"><Link className="left-items" to="/pelayanan">Pelayanan</Link></p>
+            <p className="nav-item is-hidden-mobile"><Link className="left-items" to="/penunjang">Penunjang Medis</Link></p>
+            <p className="nav-item is-hidden-mobile"><Link className="left-items" to="/karier">Karier</Link></p>
+            <p className="nav-item is-hidden-mobile"><Link className="left-items" to="/kontak">Kontak</Link></p>
             </div>
-            <div className="nav-right">
-
+            <div className="nav-right nav-menu is-active is-hidden-desktop">
+              <p className="nav-item is-hidden-tablet"><Link className="right-items" to="/news">News</Link></p>
+              <p className="nav-item is-hidden-tablet"><Link className="right-items" to="/profile">Profile</Link></p>
+              <p className="nav-item is-hidden-tablet"><Link className="right-items" to="/visi&misi">Visi & Misi</Link></p>
+              <p className="nav-item is-hidden-tablet"><Link className="right-items" to="/pelayanan">Pelayanan</Link></p>
+              <p className="nav-item is-hidden-tablet"><Link className="right-items" to="/penunjang">Penunjang Medis</Link></p>
+              <p className="nav-item is-hidden-tablet"><Link className="right-items" to="/karier">Karier</Link></p>
+              <p className="nav-item is-hidden-tablet"><Link className="right-items" to="/kontak">Kontak</Link></p>
             </div>
+            <span className="nav-toggle">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
           </nav>
 
           <div className="megatron">
@@ -69,5 +110,3 @@ class App extends React.Component {
     );
   }
 }
-
-export default App;
